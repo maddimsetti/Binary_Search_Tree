b98ff0d4b3c9c9d1e63fb8d13877a74f27ae4078
prudhvi <prudhvieethakota123@gmail.com>
2021-05-26 16:56:59 +0530

    Added: Welcome Message To Binary Search Tree Program

---
+++ src/test/java/com/binarysearch/BinarySearchTreeTest.java
@@ -0,0 +1,7 @@
+package com.binarysearch;
+
+public class BinarySearchTreeTest {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Binary Search Tree Program");
+    }
+}
